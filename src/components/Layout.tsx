c967eab47a5b24a33feeec86fe5f48cabdb8e043
Vu Le Nam <namvl@hotmail.com>
2024-04-16 00:28:19 +0900

    Add mui dependencies, add layout

---
+++ src/components/Layout.tsx
@@ -0,0 +1,170 @@
+//src/components/Layout.tsx
+import { useState } from "react"
+import { styled, useTheme } from "@mui/material/styles"
+import {
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemButton,
+  ListItemIcon,
+  Toolbar,
+  Typography,
+  IconButton,
+  Box,
+  Divider,
+} from "@mui/material"
+import CssBaseline from "@mui/material/CssBaseline"
+import { AppBar, type AppBarProps } from '@mui/material';
+import HomeIcon from "@mui/icons-material/Home"
+import InfoIcon from "@mui/icons-material/Info"
+import MenuIcon from "@mui/icons-material/Menu"
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft"
+import ChevronRightIcon from "@mui/icons-material/ChevronRight"
+
+interface LayoutProps {
+  children: React.ReactNode
+}
+const drawerWidth = 240
+
+const Main = styled("main", { shouldForwardProp: prop => prop !== "open" })<{
+  open?: boolean
+}>(({ theme, open }) => ({
+  flexGrow: 1,
+  padding: theme.spacing(3),
+  transition: theme.transitions.create("margin", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  marginLeft: `-${drawerWidth}px`,
+  ...(open && {
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  }),
+}))
+
+/**
+ * extend AppBar properties with "open" property
+ */
+interface MyAppBarProps extends AppBarProps {
+  open?: boolean
+}
+
+/**
+ * Customize AppBar
+ */
+const MyAppBar = styled(AppBar, {
+  shouldForwardProp: prop => prop !== "open",
+})<MyAppBarProps>(({ theme, open }) => ({
+  transition: theme.transitions.create(["margin", "width"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}))
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: "flex-end",
+}))
+
+function Layout({ children }: LayoutProps) {
+  const theme = useTheme()
+  const [open, setOpen] = useState(false)
+
+  const toggleDrawer = (isOpen: boolean) => () => {
+    setOpen(isOpen)
+  }
+
+  const list = (
+    <List>
+      <ListItem disablePadding>
+        <ListItemButton href="/">
+          <ListItemIcon>
+            <HomeIcon />
+          </ListItemIcon>
+          <ListItemText primary="Home" />
+        </ListItemButton>
+      </ListItem>
+      <ListItem disablePadding>
+        <ListItemButton href="/about">
+          <ListItemIcon>
+            <InfoIcon />
+          </ListItemIcon>
+          <ListItemText primary="About" />
+        </ListItemButton>
+      </ListItem>
+    </List>
+  )
+
+  return (
+    <div>
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <MyAppBar position="fixed" open={open}>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={toggleDrawer(true)}
+              edge="start"
+              sx={{ mr: 2, ...(open && { display: "none" }) }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" noWrap component="div">
+              Persistent drawer
+            </Typography>
+          </Toolbar>
+        </MyAppBar>
+        <Drawer
+          sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            "& .MuiDrawer-paper": {
+              width: drawerWidth,
+              boxSizing: "border-box",
+            },
+          }}
+          variant="persistent"
+          anchor="left"
+          open={open}
+          color="#fff"
+          PaperProps={{ style: { backgroundColor: '#3f51b5' } }} 
+        >
+          <DrawerHeader>
+            <IconButton onClick={toggleDrawer(false)}>
+              {theme.direction === "ltr" ? (
+                <ChevronLeftIcon />
+              ) : (
+                <ChevronRightIcon />
+              )}
+            </IconButton>
+          </DrawerHeader>
+          <Divider />
+          {list}
+        </Drawer>
+        <Main open={open}>
+          <DrawerHeader />
+          {children}
+        </Main>
+      </Box>
+    </div>
+  )
+}
+
+export default Layout
